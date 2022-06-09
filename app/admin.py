b48f2fab0942b4ab619e117222eea008d0f693e7
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-09 17:23:48 +0530

    's'

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Add_Vehicle)
+admin.site.register(Updated_Deisel_Price)
 admin.site.register(Route)
 admin.site.register(Back_Load_Material)
 admin.site.register(Income_Per_Route)
--- app/models.py
+++ app/models.py
@@ -23,33 +23,73 @@ class Add_Vehicle(models.Model):
     permit_tax=models.BooleanField(default=False)
 
     def __str__(self):
-        return self.user.first_name
+        return self.vehicle_number
 
 Back_Load_Material = [
     ('A', 'A'),
     ('B', 'B'),
     ('C', 'C'),
 ]
+class Updated_Deisel_Price(models.Model):
+    deisel_price=models.CharField(max_length=250,null=True,blank=True)
+
 
 class Route(models.Model):
     destination=models.CharField(max_length=250,null=True,blank=True)
     km=models.CharField(max_length=250,null=True,blank=True)
-    deisel=models.CharField(max_length=250,null=True,blank=True)
+    # deisel=models.CharField(max_length=250,null=True,blank=True)
+    # updated_deisel_price=models.ForeignKey(Updated_Deisel_Price,on_delete=models.CASCADE,null=True,blank=True)
     driver_expense=models.CharField(max_length=250,null=True,blank=True)
-    deisel_expense=models.CharField(max_length=250,null=True,blank=True)
-  
+    # deisel_expense=models.CharField(max_length=250,null=True,blank=True)
+
+    def __str__(self):
+        return self.destination
+    
 
 class Back_Load_Material(models.Model):
     back_load_material=models.CharField(max_length=250,choices=Back_Load_Material,blank=True,null=True)
     price=models.CharField(max_length=250,null=True,blank=True)
-    tonn=models.CharField(max_length=250,null=True,blank=True)
+    # tonn=models.CharField(max_length=250,null=True,blank=True)
 
 class Income_Per_Route(models.Model):
+
     vehicle=models.ForeignKey(Add_Vehicle,on_delete=models.CASCADE,null=True,blank=True)
     route=models.ForeignKey(Route,on_delete=models.CASCADE,null=True,blank=True)
     deisel=models.CharField(max_length=250,null=True,blank=True)
-    expense=models.CharField(max_length=250,null=True,blank=True)
-    earning=models.CharField(max_length=250,null=True,blank=True)
+    updated_deisel_price=models.ForeignKey(Updated_Deisel_Price,on_delete=models.CASCADE,null=True,blank=True)
+    deisel_price=models.CharField(max_length=250,null=True,blank=True)
+    extra_expenses=models.CharField(max_length=250,null=True,blank=True)
+    total_expense=models.CharField(max_length=250,null=True,blank=True)
+    payment_per_route=models.CharField(max_length=250,null=True,blank=True)
+    total_earning_per_route=models.CharField(max_length=250,null=True,blank=True)
+
     back_load=models.BooleanField(default=False)
     back_load_material=models.ForeignKey(Back_Load_Material,on_delete=models.CASCADE,null=True,blank=True)
-    tonn=models.CharField(max_length=250,null=True,blank=True)
+    tonn=models.CharField(max_length=250,null=True,blank=True,default=0)
+
+
+    def save(self, *args, **kwargs):
+        """
+        Calculate deisel price and total expenses.
+        """
+        self.deisel_price=int(self.deisel) * int(self.updated_deisel_price.deisel_price)
+        if self.extra_expenses:
+          self.total_expense = int(self.route.driver_expense) + int(self.deisel_price) +int(self.extra_expenses)
+        else:
+          self.total_expense = int(self.route.driver_expense) + int(self.deisel_price)  
+
+        self.total_earning_per_route1=int(self.payment_per_route)-int(self.total_expense)
+
+        if self.back_load:
+            self.total_earning_per_route=int(self.total_earning_per_route1)+(int(self.back_load_material.price) * int(self.tonn))
+        else:
+            self.total_earning_per_route=int(self.payment_per_route)-int(self.total_expense)
+
+        # print(self.total_earning_per_route)    
+
+        super(Income_Per_Route, self).save(*args, **kwargs)
+
+
+
+
+
